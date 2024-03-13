83a0f71afae5b8f15ef33920b1bdd7e5a7fb824b
Maverick Zhang <maverickat.17@gmail.com>
2024-03-13 22:36:03 +0800

    Eric's work

--- main.py
+++ main.py
@@ -1,56 +1,105 @@
-import sqlite3
-import pandas as pd
+import sqlite3 as sq
+import json as js
+con = sq.connect("data.db")
 
-con = sqlite3.connect("data.db")
+cur = con.cursor()
 
-df = pd.read_sql_query("SELECT * FROM  wp_posts", con)
+'''cur.execute("""CREATE TABLE customers(
+    first_name text,
+    last_name text,
+    email text
+)""")'''
 
-print(df)
-# cur.execute("SELECT post_content, ID FROM wp_posts WHERE post_content LIKE '[%' ")
+'''many_customers = [
+                    ('Wes', 'Brown', 'west@yahoo.com'), 
+                    ('Eric', 'Zhang', 'khekwok@connect.ust.hk')
+                  ]
 
-# def split(record):
-#     result = []
-#     items = ''
-#     end = 0
-#     for i in range(len(record)):
-#         if record[i] == '\n':
-#             result.append(items)
-#             items = ''
-#             continue
-#         items = items + record[i]
-#         end += 1
-#     return result
+cur.execute("INSERT INTO customers VALUES('Jason', 'Kwok', 'jk@gmail.com')")
+print("inserted")
 
-# def format(lists):
-#     result = {}
-#     flag = 0
-#     key = ''
-#     item = ''
-#     for i in range(1, len(lists)):
-#         if lists[i] == ']':
-#             flag = 1
-#             continue
-#         if flag == 0:
-#             key = key + lists[i]
-#         else:
-#             item = item + lists[i]
-#     result[key] = item
-#     return result
-
-# def makeID(input):
-#     result = []
-#     output = {}
-#     for records in input:
-#         splits = split(records[0])
-#         for i in range(len(splits)):
-#             result.append(format(splits[i]))
-#     print(output)
-
-# input = cur.fetchall()
-# print(input)
-'''makeID(input)'''
+cur.executemany("INSERT INTO customers VALUES(?, ?, ?)", many_customers)
+print("inserted many")'''
 
 
-# print(cur.fetchall())
 
-# con.close()
+cur.execute("SELECT ID, post_content FROM wp_posts WHERE post_content LIKE '[%' ")
+
+
+def split(record):
+    result = []
+    items = ''
+    end = 0
+    for i in range(len(record)):
+        if record[i] == '\n':
+            result.append(items)
+            items = ''
+            continue
+        items = items + record[i]
+        end += 1
+    return result
+
+
+def format(lists):
+    result = {}
+    flag = 0
+    key = ''
+    item = ''
+    for i in range(1, len(lists)):
+        if lists[i] == ']':
+            flag = 1
+            continue
+        if flag == 0:
+            key = key + lists[i]
+        else:
+            item = item + lists[i]
+    if key != item:
+        result[key] = item
+    return result
+
+def makeDictionary(input, db):
+    ID_dict = {}
+    for records_index in range(len(input)):
+        if db == "posts":
+            splits = split(input[records_index][1])
+        elif db == "tagthis":
+            splits = input[records_index][1]
+        output = []
+        if type(splits) is list:
+            for i in range(len(splits)):
+                result = format(splits[i])
+                if result != {}:
+                    output.append(result)
+        elif type(splits) == str:
+            result = format(splits)
+            if result != {}:
+                    output.append(result)
+        ID_dict[int(input[records_index][0])] = output
+    return ID_dict
+        
+input = cur.fetchall()
+
+output = makeDictionary(input, "posts")
+with open("result_post.json", "w") as f:
+    js.dump(output, f, indent=4)
+
+cur.execute("SELECT id, tag FROM wp_tagthis WHERE tag LIKE '[%' ")
+
+input = cur.fetchall()
+
+output = makeDictionary(input, "tagthis")
+with open("result_tagthis.json", "w") as f:
+    js.dump(output, f, indent=4)
+
+with open("result_tagthis.json", encoding="utf-8") as f:
+    print(js.load(f))
+
+
+
+con.commit()
+
+con.close()
+
+
+
+
